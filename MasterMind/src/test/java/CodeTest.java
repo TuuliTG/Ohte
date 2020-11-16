568377df9d6882bbc49302c775495396b2c83f9f
Tuuli TG <tuuli.k.toivanen@gmail.com>
2020-11-16 18:37:34 +0200

    added some tests

---
+++ MasterMind/src/test/java/CodeTest.java
@@ -0,0 +1,74 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 tgtuuli.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import jdk.internal.net.http.hpack.NaiveHuffman;
+import mastermind.domain.Code;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author tgtuuli
+ */
+public class CodeTest {
+    private Code code;
+    @Before
+    public void setUp() {
+        code = new Code();
+        code.setRandomCode();
+    }
+    
+    @Test
+    public void getCodeGivesRightSizeArray() {
+        
+        
+        assertEquals(4, code.getCode().length);
+    }
+    
+    @Test
+    public void codeHasColors() {
+        boolean hasColors = true;
+        for (int i = 0; i < 4; i++) {
+            if(!isColor(code.getCode()[i])) {
+                hasColors = false;
+            }
+        }
+        assertTrue(hasColors);
+    }
+    
+    private boolean isColor(String s) {
+        boolean isColor = false;
+        String[] colors = {"Red", "Green", "Yellow", "Blue", "Black", "White"};
+        for (int i = 0; i < colors.length; i++) {
+            if (s.equals(colors[i])) {
+                isColor = true;
+                return isColor;
+            }
+                
+        }
+        return isColor;
+    }
+    
+}
---
+++ MasterMind/src/test/java/GameLogicTest.java
@@ -0,0 +1,83 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 tgtuuli.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import mastermind.ui.GameLogic;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author tgtuuli
+ */
+public class GameLogicTest {
+    private GameLogic game;
+    
+    @Before
+    public void setUp() {
+        game = new GameLogic();
+        
+    }
+    
+    
+    @Test
+    public void feedbackTest1() {
+        String[] c = {"Blue", "Blue", "Blue", "Blue"};
+        game.setCode(c);
+        String[] g = {"Blue", "Blue", "Blue", "Blue"};
+        game.setGuess(g);
+        int[] feedback = game.getFeedback();
+        int[] expectedFeedback = {1,1,1,1};
+        assertArrayEquals(expectedFeedback, feedback);
+    }
+    
+     @Test
+    public void feedbackTest2() {
+        String[] c = {"Blue", "Red", "Yellow", "Black"};
+        game.setCode(c);
+        String[] g = {"Blue", "Blue", "Black", "White"};
+        game.setGuess(g);
+        int[] feedback = game.getFeedback();
+        int onesExp = 1;
+        int twosExp = 2;
+        int zerosExp = 1;
+        int ones = 0;
+        int twos = 0;
+        int zeros = 0;
+        
+        for (int i = 0; i < 4; i++) {
+            if (feedback[i] == 1) {
+                ones++;
+            } else if (feedback[i] == 2) {
+                twos++;
+            } else if (feedback[i] == 0) {
+                zeros++;
+            }
+        }
+         assertEquals(onesExp, ones);
+         assertEquals(twosExp, twos);
+         assertEquals(zerosExp, zeros);
+    }
+    
+}
